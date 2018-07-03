94207a557455b4ccb08df7d045f56fde60f618c7
vectronic <hello.vectronic@gmail.com>
2018-07-03 17:00:37 +0100

    feature: alpha subscription support

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Vectronic website
 `export NETLIFY_API_AUTH=<value>`
 `export NETLIFY_APPROVED_COMMENTS_FORM_ID=<value>`
 `export SLACK_COMMENT_WEBHOOK_URL=<value>`
+`export SENDINBLUE_API_AUTH=<value>`
+`export SENDINBLUE_LIST_ID=<value>`
 `npx netlify-lambda serve src/functions`
 `hugo server -w --disableFastRender --ignoreCache`
 
---
+++ src/functions/subscription.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let request = require("request");
+
+// populate environment variables locally.
+require('dotenv').config();
+
+// Handle the lambda invocation
+exports.handler = function(event, context, callback) {
+
+    // get the arguments from the notification
+    let body = JSON.parse(event.body);
+
+    // prepare call to the Sendinblue API
+    let sendinblueURL = 'https://api.sendinblue.com/v3/contacts';
+    let sendinbluePayload = {
+        'listIds': [process.env.SENDINBLUE_LIST_ID],
+        'email': body.data.email,
+        'attributes': [
+            {
+                'FIRSTNAME': body.data.first_name
+            },
+            {
+                'LASTNAME': body.data.last_name
+            }
+        ]
+    };
+
+    let headers = {
+        'content-type': 'application/json',
+        'api-key': process.env.SENDINBLUE_API_AUTH
+    };
+
+    // post the notification to Send In Blue
+    request.post({url:sendinblueURL, json: sendinbluePayload, headers: headers}, function(err, httpResponse, body) {
+        let msg;
+        if (err) {
+            msg = 'Post to Sendinblue failed:' + err;
+        }
+        else {
+            msg = 'Post to Sendinblue successful!  Server responded with:' + body;
+        }
+        callback(null, {
+            statusCode: 200,
+            body: msg
+        });
+        return console.log(msg);
+    });
+};
